570de257649703703cbd20a680bdd7149926d8ea
Jon <Jonathan.Kreiter@outlook.com>
2020-09-21 17:11:46 -0700

    eslintfiles issue faq question

--- utils/faq.js
+++ utils/faq.js
@@ -6,7 +6,7 @@
 const faq = [
     {question: 'can I import an alias file even though a eslint config exists in my project directory?', 
     answer: 'No. You will need to either save your eslint config or delete it. The alias file is renamed as \'eslint.rc*\' when it is imported. If one exists, it will not import.'},
-    {question: 'will my saved configs be delete with every update?', answer: 'No they won\'t be deleted.'},
+    {question: 'will my saved configs be delete with every update?', answer: 'They will be deleted. Be careful!'},
 ]
 
 module.exports = faq; 
---
+++ utils/fawUI.js
@@ -0,0 +1,1 @@
+const faqUI = require('cliui');
